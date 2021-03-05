ae8eafeaf30d4c345fa4aee3c04ac17b0c28b0fa
sonu <sonuk@mashreq.com>
2021-03-05 23:27:45 +0400

    update dockerfile and properties file

--- src/test/java/com/eservice/portal/UserControllerTests.java
+++ src/test/java/com/eservice/portal/UserControllerTests.java
@@ -1,6 +1,6 @@
 package com.eservice.portal;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
